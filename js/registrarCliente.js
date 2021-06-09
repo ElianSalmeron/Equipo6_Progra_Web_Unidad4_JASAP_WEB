9c7daa5e15d09a2ce54a79dee0a122940de8678e
ElianSalmeron <79241751+ElianSalmeron@users.noreply.github.com>
2021-06-08 21:23:56 -0500

    Add files via upload

--- js/registrarCliente.js
+++ js/registrarCliente.js
@@ -1,4 +1,4 @@
-function validaDatosCliente(correo, telefono){
+function validaDatosCliente(correo, telefono, password, func){
 
     var correo_patron = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"
     var telefono_patron = "[1-9]{1}[0-9]{9}";
@@ -16,8 +16,16 @@ function validaDatosCliente(correo, telefono){
             msg += "\nFormato incorrecto en teléfono";
     }
 
+    if(password.value.length < 8 || password.value.length > 12)
+        msg += "\nLa contraseña debe tener entre 8 y 12 caracteres";
+
     if(msg == ""){
-        alert("Te has registrado correctamente");
+
+        if(func == 0)
+            alert("Se han actualizado los datos correctamente");
+        else
+            alert("Te has registrado correctamente");
+            
         confirm = true;
     }
 	else
